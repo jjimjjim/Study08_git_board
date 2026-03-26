4d2548908fc12cce22e696d1bba39344fc76001b
표예찬 <yechan0807@naver.com>
2026-03-26 14:12:05 +0900

    로그인 기능 추가

--- src/main/java/com/kedu/controllers/MemberController.java
+++ src/main/java/com/kedu/controllers/MemberController.java
@@ -1,10 +1,30 @@
 package com.kedu.controllers;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.kedu.commons.EncryptionUtils;
+import com.kedu.dao.MemberDAO;
 
 @Controller
 @RequestMapping("/member")
 public class MemberController {
+	
+	@Autowired
+	private MemberDAO dao;
+	
+	private EncryptionUtils eu = new EncryptionUtils();
 
+	@RequestMapping("/login")
+	public String login(String id, String pw,HttpSession session) {
+		boolean result = dao.login(id,eu.getSha512(pw));
+		if(result) {
+			session.setAttribute("loginId", id);
+		}
+		return "redirect:/";	
+	}
+	
 }
--- src/main/java/com/kedu/dao/MemberDAO.java
+++ src/main/java/com/kedu/dao/MemberDAO.java
@@ -9,4 +9,16 @@ import org.springframework.stereotype.Repository;
 public class MemberDAO {
 	@Autowired
 	private JdbcTemplate jdbc;
+	
+	public boolean login(String id, String pw) {
+		String sql = "select count(*) from member where id = ? and pw = ?";
+		int result = jdbc.queryForObject(sql,Integer.class,id,pw);
+		if(result > 0) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+	
+	
 }
